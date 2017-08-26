9da9d61d7059b0a4881ffea5eda4967538a514bd
karynariepka <karyna.riepka@gmail.com>
2017-08-26 13:12:19 -0400

    initial

---
+++ src/App.java
@@ -0,0 +1,8 @@
+
+public class App {
+//	String webFirstName = settingsPage.userFirstName.getAttribute("value");
+//	String webLastName = settingsPage.userLastName.getAttribute("value");
+//	String webEmail = settingsPage.userEmail.getAttribute("value").split("@")[0];
+//	String webPhoneNumber = settingsPage.userPhoneNumber.getAttribute("value");
+
+}
